3742d48f2468978d773776db0816ede665439615
Nitya Tarakad <nitarakad@gmail.com>
2021-02-26 13:22:16 -0500

    added grad accum, changed to use stack, added attention masks

--- MyDPRTrainer.py
+++ MyDPRTrainer.py
@@ -48,33 +48,38 @@ def traversal_for_question_context(query, highest_doc, lowest_doc, other_gold_do
     6. NLL the 0th component (positive example)
     7. return the loss
     '''
-    # zero the gradient
-    question_model.zero_grad()
-    context_model.zero_grad()
 
     # query embeddings
-    query_input_ids = question_tokenizer(query, return_tensors='pt')["input_ids"].cuda()
-    query_embeddings = question_model(query_input_ids).pooler_output.cuda()
+    question_tokenized = question_tokenizer(query, return_tensors='pt')
+    query_input_ids = question_tokenized["input_ids"].cuda()
+    query_attn_mask = question_tokenized["attention_mask"].cuda()
+    query_embeddings = question_model(query_input_ids, attention_mask=query_attn_mask).pooler_output.cuda()
 
     #highest doc/lowest doc embeddings
-    highest_doc_input_ids = context_tokenizer(highest_doc, return_tensors='pt')["input_ids"].cuda()
-    highest_doc_embeddings = context_model(highest_doc_input_ids).pooler_output.cuda()
-    lowest_doc_input_ids = context_tokenizer(lowest_doc, return_tensors='pt')["input_ids"].cuda()
-    lowest_doc_embeddings = context_model(lowest_doc_input_ids).pooler_output.cuda()
+    highest_doc_tokenized = context_tokenizer(highest_doc, return_tensors='pt')
+    highest_doc_input_ids = highest_doc_tokenized["input_ids"].cuda()
+    highest_doc_attn_mask = highest_doc_tokenized["attention_mask"].cuda()
+    highest_doc_embeddings = context_model(highest_doc_input_ids, attention_mask=highest_doc_attn_mask).pooler_output.cuda()
+    lowest_doc_tokenized = context_tokenizer(lowest_doc, return_tensors='pt')
+    lowest_doc_input_ids = lowest_doc_tokenized["input_ids"].cuda()
+    lowest_doc_attn_mask = lowest_doc_tokenized["attention_mask"].cuda()
+    lowest_doc_embeddings = context_model(lowest_doc_input_ids, attention_mask=lowest_doc_attn_mask).pooler_output.cuda()
 
     # other gold doc embeddings
     other_gold_doc_embeddings = [highest_doc_embeddings, lowest_doc_embeddings]
     for gold_doc in other_gold_docs:
-        gold_doc_input_ids = context_tokenizer(gold_doc, return_tensors='pt')["input_ids"].cuda()
-        gold_doc_embeddings = context_model(gold_doc_input_ids).pooler_output
+        tokenized = context_tokenizer(gold_doc, return_tensors='pt')
+        gold_doc_input_ids = tokenized["input_ids"].cuda()
+        gold_doc_attn_mask = tokenized["attention_mask"].cuda()
+        gold_doc_embeddings = context_model(gold_doc_input_ids, attention_mask=gold_doc_attn_mask).pooler_output
         other_gold_doc_embeddings.append(gold_doc_embeddings)
-    other_gold_doc_embeddings = torch.cat(other_gold_doc_embeddings)
+    other_gold_doc_embeddings = torch.stack(other_gold_doc_embeddings).cuda()
 
     # make matrix of query embedding
     query_embeddings_arr = []
     for i in range(16):
         query_embeddings_arr.append(query_embeddings)
-    query_embeddings_matrix = torch.cat(query_embeddings_arr).cuda()
+    query_embeddings_matrix = torch.stack(query_embeddings_arr).cuda()
 
     # take dot product
     matrix_mult = torch.matmul(query_embeddings_matrix, other_gold_doc_embeddings.T)
@@ -90,7 +95,7 @@ def traversal_for_question_context(query, highest_doc, lowest_doc, other_gold_do
     return loss
 
 
-def train_dpr(num_epochs=1, see_loss_step=50, save_model_step=500):
+def train_dpr(num_epochs=1, see_loss_step=50, save_model_step=500, grad_accum=32):
     # loop through all epochs
     for i in range(num_epochs):
         print("EPOCH: ", i)
@@ -103,7 +108,7 @@ def train_dpr(num_epochs=1, see_loss_step=50, save_model_step=500):
             {'params': [p for n, p in question_model.named_parameters()]},
             {'params': [p for n, p in context_model.named_parameters()]}
         ]
-        optimizer = AdamW(optimizer_grouped_parameters, lr=1e-5)
+        optimizer = AdamW(optimizer_grouped_parameters, lr=5e-6)
         # loop through all values in the dataset
         for idx in tqdm(range(len(train_dataset)), total=len(train_dataset), desc="running through training data"):
             #print(len(train_dataset[idx]))
@@ -112,9 +117,13 @@ def train_dpr(num_epochs=1, see_loss_step=50, save_model_step=500):
                 loss = traversal_for_question_context(query, high_d, low_d, other_d)
                 total_loss += loss
                 loss.backward()
-                optimizer.step()
+                if idx % grad_accum == 0:
+                    optimizer.step()
+                    # zero the gradient
+                    question_model.zero_grad()
+                    context_model.zero_grad()
                 if idx % see_loss_step == 0:
-                    print("Loss within curr epoch at " + str(idx) + ":", total_loss.detach())
+                    print("Loss within curr epoch at " + str(idx) + ":", loss.detach())
                  # save model
                 if idx % save_model_step == 0:
                     print("saving model at epoch: " + str(i))
